d35a224f453a9974ca0f488cbe592c7f5210dfb5
tanggalabang <tanggalabang26@gmail.com>
2023-06-11 07:07:28 +0700

    video ke-8

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -152,7 +152,7 @@
           <img src="{{ url('dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">{{ Auth::user()->name }}</a>
         </div>
       </div>
 
@@ -161,6 +161,8 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
+
+          @if( Auth::user()->user_type == 1)
           <li class="nav-item">
             <a href="{{ url('admin/dashboard') }}" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -174,6 +176,47 @@
               <i class="nav-icon far fa-user"></i>
               <p>
                 Admin
+              </p>
+            </a>
+          </li>
+
+          @elseif( Auth::user()->user_type == 2)
+          <li class="nav-item">
+            <a href="{{ url('teacher/dashboard') }}" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+
+          @elseif( Auth::user()->user_type == 3)
+          <li class="nav-item">
+            <a href="{{ url('student/dashboard') }}" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+
+          @elseif( Auth::user()->user_type == 4)
+          <li class="nav-item">
+            <a href="{{ url('parent/dashboard') }}" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+
+          @endif
+
+          <li class="nav-item">
+            <a href="{{ url('logout') }}" class="nav-link">
+              <i class="nav-icon far fa-user"></i>
+              <p>
+               Logout
               </p>
             </a>
           </li>
